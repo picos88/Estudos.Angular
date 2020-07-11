0d4ca7e999f8e7af50505e96b7e81151016902dd
Pietro.Cosentino <pietrocosentino88@gmail.com>
2020-07-11 17:03:48 -0300

    Projeto Angular1

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+   ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'course', pathMatch: 'full'
+      }
+     
+    ])
+  ],
+  
+    providers: [],
+    bootstrap: [AppComponent]
+})
+  
+export class AppModule { }
